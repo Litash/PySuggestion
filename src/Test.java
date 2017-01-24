0780f81f076e7f440ec65ccc215420bafdbb182e
Yichen Lu <litash529@gmail.com>
2017-01-24 17:07:02 +0000

    initial commit; project has been setup

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by Litash on 1/24/2017.
+ */
+public class Test extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
